49b7b8c1baf4f784a627ccdc9ce0cc47bf7acbb1
MinaMorkos-M <63639438+MinaMorkos-M@users.noreply.github.com>
2021-01-08 02:21:02 +0200

    database queries are changed

--- notifications.sql
+++ notifications.sql
@@ -24,15 +24,8 @@ CREATE TABLE mail (
   primary key (id)
 );
 
-INSERT INTO templates(id , subject , content , language)VALUES('e9daf10c-10cb-4ec4-aac9-464993472c1c', 'mina', 'morkos', 'ENGLISH');
-INSERT INTO templates(id , subject , content , language)VALUES('0892d944-6bf2-4970-875d-9385a1795fe4', 'philo', 'morkos', 'ENGLISH');
-INSERT INTO templates(id , subject , content , language)VALUES('1927f37e-c02d-4774-a502-493631fc263a', 'fady', 'morkos', 'ENGLISH');
-
 
 SELECT * FROM templates;
 SELECT * FROM sms;
 SELECT * FROM mail;
 
-DROP TABLE templates;
-DROP TABLE sms;
-DROP TABLE mail;
